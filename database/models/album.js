662eff30fd66b9889d15d90482f29494e0b082fe
Patel-Preet <pp225622@gmail.com>
2023-05-04 18:22:15 +0800

    app complete, mongo doesnt connect, app not workin

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,25 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    title: {
+      type: String,
+    },
+    date: {
+        type: Date
+    },
+    copiesSold: {
+        type: Number
+    },
+    numberTracks:{
+        type: Number
+    },
+    image:{
+        type: String
+    },
+    revenue: {
+        type: Number
+    }
+});
+
+module.exports = AlbumSchema;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,38 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./artist');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+    name: {
+      type: String
+    },
+    age: {
+        type: Number
+    },
+    yearsActive: {
+        type: Number
+    },
+    image:{
+        type: String
+    },
+    genre: {
+        type: String
+    },
+    website:{
+        type: String
+    },
+    netWorth: {
+        type: Number
+    },
+    labelName: {
+        type: String
+    },
+    retired: {
+        type: Boolean
+    },
+    albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
